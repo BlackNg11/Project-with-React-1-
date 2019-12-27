010152436b3bab541e1fd182cc83eb84d640af9a
Black <blackng10.5.1997@gmail.com>
2019-12-27 17:42:00 +0700

    finish carrousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './resources/styles.css';
+
 import Header from './component/header_footer/header';
+import Featured from './component/featured';
 
 function App() {
   return (
     <div className="App" style={{height: "1500px",background:"cornflowerblue"}}>
       <Header />
+      <Featured />
     </div>
   );
 }
---
+++ src/component/featured/carrousel.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Slider from "react-slick";
+
+import slide_one from '../../resources/images/slide_one.jpg';
+import slide_two from '../../resources/images/slide_two.jpg';
+import slide_three from '../../resources/images/slide_three.jpg';
+
+const Carrousel = () => {
+  const setting = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500
+  };
+
+  return(
+    <div
+      className="carrousel_wrapper"
+      style={{
+        overflow: "hidden",
+        height: /*`$(window.innerHeight)px`*/ "100vh"
+      }}
+    >
+      <Slider {...setting}>
+        <div>
+          <div className="carrousel_image" style= {{
+            background: `url(${slide_one})`,
+            height: '100vh'
+          }}>
+           </div>
+        </div>
+        <div>
+          <div className="carrousel_image" style= {{
+            background: `url(${slide_two})`,
+            height: '100vh'
+          }}>
+           </div>
+        </div>
+        <div>
+          <div className="carrousel_image" style= {{
+            background: `url(${slide_three})`,
+            height: '100vh'
+          }}>
+         </div>
+        </div>
+      </Slider>
+
+
+    </div>
+  );
+};
+
+export default Carrousel;
